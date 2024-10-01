544699de4af9748a744ac3dd859330358c746a3a
unknown <gilangpermana1407@gmail.com>
2024-10-01 11:25:06 +0700

    fix yarn

--- pages/components/charts/SiteIdDetailTotal.tsx
+++ pages/components/charts/SiteIdDetailTotal.tsx
@@ -1,88 +1,65 @@
 import { ApexOptions } from "apexcharts";
 import dynamic from "next/dynamic";
-import { Component } from "react";
+import { useState } from "react";
+
 const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-interface spark3 {
-  options?: ApexOptions,
-  width?: number;
-  height?: string | number,
-  series?: ApexOptions['series'],
-  label?: XAxisAnnotations
-  color?: string | string[] | (string & string[]) | undefined
-  endingShape?: string
-  enabled?: boolean;
-}
+const SiteIdDetailTotal = () => {
+  const [series, setSeries] = useState([32, 27, 25, 16]);
+  const [options, setOptions] = useState<ApexOptions>({
+    chart: {
+      events: {
+        mounted: (chart) => {
+          chart.windowResizeHandler();
+        },
+      },
+      type: 'donut',
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+    stroke: {
+      show: true,
+      curve: 'smooth',
+      lineCap: 'round',
+      colors: ["#fff"],
+      width: 0,
+      dashArray: 0,
+    },
+    plotOptions: {
+      pie: {
+        expandOnClick: false,
+        donut: {
+          size: '72%',
+          labels: {
+            show: false,
+            name: {
+              show: true,
+              fontSize: '20px',
+              color: '#495057',
+              offsetY: -4,
+            },
+            value: {
+              show: true,
+              fontSize: '18px',
+              offsetY: 8,
+              formatter: (val) => `${val}%`,
+            },
+          },
+        },
+      },
+    },
+    colors: ["#FBA57E", "#8AFFF9", "#FEE18E", "#FF67B1"],
+  });
 
-export class SiteIdDetailTotal extends Component<{}, spark3> {
-  constructor(props: {} | Readonly<{}>) {
-      super(props);
+  return (
+    <ReactApexChart options={options} series={series} type="donut" height={250} />
+  );
+};
 
-      this.state = {
-          series: [32, 27, 25, 16],
-          options: {
+export default SiteIdDetailTotal;
 
-              // labels: ["My First Datset"],
-              chart: {
-                  events: {
-                      mounted: (chart) => {
-                        chart.windowResizeHandler();
-                      }
-                    },
-                  type: 'donut'
-              },
-              dataLabels: {
-                  enabled: false,
-              },
-
-              legend: {
-                  show: false,
-              },
-              stroke: {
-                  show: true,
-                  curve: 'smooth',
-                  lineCap: 'round',
-                  colors: ["#fff"],
-                  width: 0,
-                  dashArray: 0,
-              },
-              plotOptions: {
-
-                  pie: {
-                      expandOnClick: false,
-                      donut: {
-                          size: '72%',
-                          labels: {
-                              show: false,
-                              name: {
-                                  show: true,
-                                  fontSize: '20px',
-                                  color: '#495057',
-                                  offsetY: -4
-                              },
-                              value: {
-                                  show: true,
-                                  fontSize: '18px',
-                                  color: undefined,
-                                  offsetY: 8,
-                                  formatter: function (val) {
-                                      return val + "%";
-                                  }
-                              },
-
-                          }
-                      }
-                  }
-              },
-              colors: ["#FBA57E",  "#8AFFF9", "#FEE18E", "#FF67B1"],
-          }
-
-      };
-  }
-
-  render() {
-      return (
-          <ReactApexChart options={this.state.options} series={this.state.series} type="donut" height={250} />
-      );
-  }
-}
+SiteIdDetailTotal.layout = "Contentlayout";
